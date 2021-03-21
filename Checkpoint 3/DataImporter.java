8b765b269562d5d64effb9e36dba7cc9c79d6f4e
Dalen Ledingham <80103222+Quaice714@users.noreply.github.com>
2021-03-20 21:14:30 -0500

    Update DataImporter.java
    Updated DataImporter.java with working test methods. Will use these as a guide for final methods to be submitted.

--- Checkpoint 3/DataImporter.java
+++ Checkpoint 3/DataImporter.java
@@ -1,5 +1,6 @@
 import java.util.*;
 import java.sql.*;
+import java.io.*;
 
 /**
 * CPSC 5133 - Database II
@@ -9,61 +10,70 @@ import java.sql.*;
 */
 
 public class DataImporter {
-  
-  private Connection connect() {
-    String url = "jdbc:sqlite:C://sqlite/hospital-database.sl3";
-    Connection conn = null;
-    try {
-      conn = DriverManager.getConnection(url);
-      
-      conn.createStatement().executeUpdate("PRAGMA foreign_keys = ON;");
-    }
-    catch (SQLException e) {
-      System.out.println(e.getMessage()); 
-    }
-    
-    return conn;
-  }
-  
-  public void selectSample() {
-    String sql = "SELECT snum, sname FROM suppliers;";
-    
-    try (Connection conn = this.connect();
-        Statement stmt = conn.createStatement();
-        ResultSet rs = stmt.executeQuery(sql)) {
-       
-      while (rs.next()) {
-        System.out.println(rs.getString("snum") + "\t" + rs.getString("sname") + "\t"); 
+
+   ArrayList<String> data = new ArrayList<String>();
+   
+   private Connection connect() {
+      String url = "jdbc:sqlite:C://sqlite/test.db";
+      Connection conn = null;
+      try {
+         conn = DriverManager.getConnection(url);
+         
+         conn.createStatement().executeUpdate("PRAGMA foreign_keys = ON;");
       }
-    }
-    catch (SQLException e) {
-      System.out.println(e.getMessage()); 
-    }
-  }
-  
-  public void insertSample(String snum, String sname, int status, String city) {
-    String sql = "INSERT INTO Suppliers(snum, sname, status, city) VALUES (?,?,?,?);";
-    
-    try (Connection conn = this.connect();) {
-      PreparedStatement ps = conn.prepareStatement(sql);
-      ps.setString(1, snum);
-      ps.setString(2, sname);
-      ps.setInt(3, status);
-      ps.setString(4, city);
-      ps.executeUpdate();
-      ps.close();
-    }
-    catch (SQLException e) {
-      System.out.println(e.getMessage()); 
-    }
-  }
-  
-  public static void main(String[] args) {
-    DataImporter app = new DataImporter();
-    app.selectSample();
-    app.insertSample("7", "Michael", 45, "Auburn");
-    System.out.prinln("After Insert");
-    app.selectSample();
-  }
-  
+      catch (SQLException e) {
+         System.out.println(e.getMessage()); 
+      }
+      
+      return conn;
+   }
+   
+   // Read data from file -- use scanner and store in arraylist
+   public void readFile(String filePath) {
+      try (Scanner scan = new Scanner(new File(filePath))) {
+         scan.useDelimiter(",");
+         String line = null;
+         String[] split = null;
+         
+         while (scan.hasNextLine()) {
+            line = scan.nextLine();
+            split = line.split(",");
+            ArrayList<String> data = new ArrayList<String>();
+            
+            for (String text : split) {
+               data.add(text);
+            }
+            
+            System.out.println(line);
+            handleInput(data);
+         }
+                     
+         scan.close();
+      }
+      catch (IOException e) {
+         System.out.println(e.getMessage());
+      }
+   }
+   
+   public void handleInput(ArrayList<String> arr) {
+      String sql = "INSERT INTO Names(type, firstName, lastName) VALUES (?,?,?);";
+   
+      try (Connection conn = this.connect();) {
+         PreparedStatement ps = conn.prepareStatement(sql);
+         ps.setString(1, arr.get(0));
+         ps.setString(2, arr.get(1));
+         ps.setString(3, arr.get(2));
+         ps.executeUpdate();
+         ps.close();
+      }
+      catch (SQLException e) {
+         System.out.println(e.getMessage());
+      
+      }
+   }
+   
+   public static void main(String[] args) {
+      DataImporter app = new DataImporter();
+      app.readFile("H:\\Auburn CPSC\\CPSC 5133 - Database II\\person-data.txt");
+   }
 }
